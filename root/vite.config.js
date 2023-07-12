a36782d9e94484f4c7d38aaf1686410e6aade0c1
Santiago Orozco <dev.orozcoh@gmail.com>
2023-07-12 12:30:22 -0500

    ...

--- root/vite.config.js
+++ root/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Plaga/",
+  //base: "/Plaga/",
   plugins: [react()],
   build: {
     outDir: "../",
